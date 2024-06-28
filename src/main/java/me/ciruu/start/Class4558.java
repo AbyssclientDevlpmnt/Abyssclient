1f59b08dd2c9f82395a7842ff9bcf24236371f36
Abyssclient - Development <143216072+AbyssclientDevlpmnt@users.noreply.github.com>
2024-06-28 07:08:43 -0500

    Add files via upload

---
+++ src/main/java/me/ciruu/start/Class4558.java
@@ -0,0 +1,13 @@
+package me.ciruu.start;
+
+public enum Class4558 {
+    Field9195(1, 2, 1, 5),
+    Field9196(2, 5, 8, 6),
+    Field9197(1, 8, 3, 9),
+    Field9198(3, 6, 9, 3),
+    Field9199(6, 3, 4, 8);
+
+    Class4558(int i, int i1, int i2, int i3) {
+
+    }
+}
---
+++ src/main/java/me/ciruu/start/Class4559.java
@@ -0,0 +1,22 @@
+package me.ciruu.start;
+
+import me.ciruu.abyss.Class488;
+import me.ciruu.abyss.AbyssMod;
+
+public class Class4559 extends Class488.CLASS488_VI {
+    public static void Method4560(Class4558 clazz) {
+        AbyssMod.INSTANCE = new AbyssMod();
+        Class4558.Field9199.name().getBytes();
+        Class4558.Field9199.name().toLowerCase();
+        Class4558.Field9199.name().concat(String.valueOf(Class4558.Field9199) + "1");
+        Class4558.Field9195.name().getBytes();
+        Class4558.Field9195.name().toLowerCase();
+        Class4558.Field9195.name().concat(String.valueOf(Class4558.Field9195) + "1");
+        Class4558.Field9196.name().getBytes();
+        Class4558.Field9196.name().toLowerCase();
+        Class4558.Field9196.name().concat(String.valueOf(Class4558.Field9196) + "1");
+        Class4558.Field9197.name().getBytes();
+        Class4558.Field9197.name().toLowerCase();
+        Class4558.Field9197.name().concat(String.valueOf(Class4558.Field9197) + "1");
+    }
+}
