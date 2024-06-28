a982d23230e85811df3c07f35b8c28e105bfba21
Abyssclient - Development <143216072+AbyssclientDevlpmnt@users.noreply.github.com>
2024-06-28 06:59:02 -0500

    Add files via upload

---
+++ src/main/java/me/ciruu/abyss/enums/Class600.java
@@ -0,0 +1,36 @@
+package me.ciruu.abyss.enums;
+
+public enum Class600 {
+    BLACK("BLACK", '0'),
+    DARK_BLUE("DARK_BLUE", '1'),
+    DARK_GREEN("DARK_GREEN", '2'),
+    DARK_AQUA("DARK_AQUA", '3'),
+    DARK_RED("DARK_RED", '4'),
+    DARK_PURPLE("DARK_PURPLE", '5'),
+    GOLD("GOLD", '6'),
+    GRAY("GRAY", '7'),
+    DARK_GRAY("DARK_GRAY", '8'),
+    BLUE("BLUE", '9'),
+    GREEN("GREEN", 'a'),
+    AQUA("AQUA", 'b'),
+    RED("RED", 'c'),
+    LIGHT_PURPLE("LIGHT_PURPLE", 'd'),
+    YELLOW("YELLOW", 'e'),
+    WHITE("WHITE", 'f');
+
+    public String Field2513;
+    public static String Field2514;
+
+    /*
+     * WARNING - void declaration
+     */
+    private Class600() {
+    }
+
+    Class600(String darkGreen, char c) {
+    }
+
+    public String toString() {
+        return this.Field2514;
+    }
+}
