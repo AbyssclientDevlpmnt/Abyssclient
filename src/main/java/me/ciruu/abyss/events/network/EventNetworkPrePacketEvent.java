6a8829b159f67d6d44effa950655fccedee2913e
Abyssclient - Development <143216072+AbyssclientDevlpmnt@users.noreply.github.com>
2024-06-28 06:54:45 -0500

    Add files via upload

---
+++ src/main/java/me/ciruu/abyss/events/network/EventNetworkPrePacketEvent.java
@@ -0,0 +1,18 @@
+package me.ciruu.abyss.events.network;
+
+import me.ciruu.abyss.events.network.EventNetworkPacketEvent;
+import net.minecraft.network.Packet;
+
+/*
+ * Exception performing whole class analysis ignored.
+ */
+public class EventNetworkPrePacketEvent
+extends EventNetworkPacketEvent {
+    public EventNetworkPrePacketEvent(Packet packet) {
+        super(packet);
+    }
+
+    public Object Method49() {
+        return Method49();
+    }
+}
---
+++ src/main/java/me/ciruu/abyss/events/player/EventPlayerMotionUpdate.java
@@ -0,0 +1,11 @@
+package me.ciruu.abyss.events.player;
+
+import me.ciruu.abyss.enums.Class53;
+import me.ciruu.abyss.events.player.EventPlayerUpdateWalking;
+
+public class EventPlayerMotionUpdate
+extends EventPlayerUpdateWalking {
+    public EventPlayerMotionUpdate(Class53 class53, float f, float f2) {
+        super(class53, 0.0, 0.0, 0.0, false);
+    }
+}
