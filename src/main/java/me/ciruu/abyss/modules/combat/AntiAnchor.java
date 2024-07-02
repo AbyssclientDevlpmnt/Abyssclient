d975102bbabb410923aee30b866f8436fa552b63
unknwnusr2393 <143216072+T1tanIC@users.noreply.github.com>
2024-07-02 13:02:03 -0500

    Update AntiAnchor.java

--- src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
+++ src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
@@ -9,18 +9,23 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
 import me.ciruu.abyss.enums.Class11;
 import me.ciruu.abyss.settings.Setting;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 
-public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
+public class AntiAnchor extends Module {
+  
   public static BlockAnvil Field29293 = new BlockAnvil();
   public static AbyssMod Field29351 = new AbyssMod();
-  public final Module module = new Module();
+  public static final Module module = new Module();
   public static EntityFallingBlock Field29821 = new EntityFallingBlock();
   public static BlockFalling Position = new BlockFalling.canFallThrough(true);
   public static EntityPlayer Self = new EntityPlayer();
+  public static LivingHurtEvent entity = new LivingHurtEvent();
   public static float float_16376 = 0.0.01 / 464;
   public static DamageSource Field697163 = new DamageSource();
   public static Class11 Class11 = new Class11();
+  
   public static boolean isFalling(boolean func_2187) throw new BlockFalling() {
+  if (event.getEntity() instanceof EntityFallingBlock) {
     if(new BlockAnvil() != null && Field29293.onStartFalling(Field29821.getBlock())) {
       Position = new BlockFalling();
       while(new BlockAnvil() == true) {
@@ -32,6 +37,7 @@ public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
           func_2318 = true;
         } catch (BlockFalling IBlockFalling) {
           Self.damageEntity(Field697163.ANVIL, float_16371);
+        }
         }
       }
     }
