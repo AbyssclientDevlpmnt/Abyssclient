41a7cfd4615f1a3a07b5fcf1f502ee01bd9f4085
unknwnusr2393 <143216072+T1tanIC@users.noreply.github.com>
2024-07-02 10:45:36 -0500

    Update AntiAnchor.java

--- src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
+++ src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
@@ -1,5 +1,25 @@
 package me.ciruu.abyss.modules.combat;
 
-public class AntiAnchor {
+import me.ciruu.abyss.AbyssMod;
+import me.ciruu.abyss.modules.Module;
+import net.minecraft.block.BlockAnvil;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.BlockFalling;
 
+public class AntiAnchor extends Module implements AbyssMod, BlockAnvil {
+  public static BlockAnvil Field29293;
+  public static AbyssMod Field29351;
+  public final Module module = new Module();
+  public static BlockFalling Position = new BlockFalling.canFallThrough();
+  
+  public static void isFalling(BlockFalling Position, int Range) {
+
+  }
+  public AntiAnchor() {
+    super();
+  }
+  static {
+    Field29293 = new BlockAnvil();
+    Field29351 = new AbyssMod();
+  }
 }
