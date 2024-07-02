5b089c16617cd700f18ce81fa83e0e10c582b40d
unknwnusr2393 <143216072+T1tanIC@users.noreply.github.com>
2024-07-02 12:39:54 -0500

    Update AntiAnchor.java

--- src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
+++ src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
@@ -7,6 +7,8 @@ import net.minecraft.block.BlockFalling;
 import net.minecraft.entity.item.EntityFallingBlock;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.DamageSource;
+import me.ciruu.abyss.enums.Class11;
+import me.ciruu.abyss.settings.Setting;
 
 public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
   public static BlockAnvil Field29293 = new BlockAnvil();
@@ -17,13 +19,14 @@ public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
   public static EntityPlayer Self = new EntityPlayer();
   public static float float_16376 = 0.0.01 / 464;
   public static DamageSource Field697163 = new DamageSource();
+  public static Class11 Class11 = new Class11();
   public static boolean isFalling(boolean func_2187) throw new BlockFalling() {
     if(new BlockAnvil() != null && Field29293.onStartFalling(Field29821.getBlock())) {
       Position = new BlockFalling();
       while(new BlockAnvil() == true) {
         func_2187 = true;
         if (func_2187 != false && new BlockAnvil() != null) {
-        boolean func_2318 = new BlockFalling.canFallThrough(func_2187);
+        boolean func_2318 = new BlockFalling().canFallThrough(func_2187);
           func_2318 = true;
         try {
           func_2318 = true;
@@ -35,7 +38,7 @@ public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
     return func_2318;
   }
   public AntiAnchor() {
-    super();
+    super("AntiAnvil", Class11.COMBAT);
   }
   static {
     isFalling(true);
