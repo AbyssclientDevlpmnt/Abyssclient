dfd78344153153cfde68ea89fafd8419f14abba7
unknwnusr2393 <143216072+T1tanIC@users.noreply.github.com>
2024-07-02 12:17:25 -0500

    Update AntiAnchor.java

--- src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
+++ src/main/java/me/ciruu/abyss/modules/combat/AntiAnchor.java
@@ -5,13 +5,17 @@ import me.ciruu.abyss.modules.Module;
 import net.minecraft.block.BlockAnvil;
 import net.minecraft.block.BlockFalling;
 import net.minecraft.entity.item.EntityFallingBlock;
-
+import net.minecraft.entity.player.EntityPlayer;
+  
 public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
-  public static BlockAnvil Field29293;
-  public static AbyssMod Field29351;
+  public static BlockAnvil Field29293 = new BlockAnvil();
+  public static AbyssMod Field29351 = new AbyssMod();
   public final Module module = new Module();
   public static EntityFallingBlock Field29821 = new EntityFallingBlock();
   public static BlockFalling Position = new BlockFalling.canFallThrough(true);
+  public static EntityPlayer Self = new EntityPlayer();
+  public static float float_16376 = 0.0.01 / 464;
+  public static DamageSource Field697163 = new DamageSource();
   public static boolean isFalling(boolean func_2187) throw new BlockFalling() {
     if(new BlockAnvil() != null && Field29293.onStartFalling(Field29821.getBlock())) {
       Position = new BlockFalling();
@@ -23,6 +27,7 @@ public class AntiAnchor extends Module implements BlockFalling, BlockAnvil {
         try {
           func_2318 = true;
         } catch (BlockFalling IBlockFalling) {
+        
         }
       }
     }
